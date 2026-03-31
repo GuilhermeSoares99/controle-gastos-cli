332f88f077d89f088057522fc9a75d555abf2807
GuilhermeSoares99 <guilherme.gs@sempreceub.com>
2026-03-31 15:45:27 -0300

    Update test_gastos.py

--- tests/test_gastos.py
+++ tests/test_gastos.py
@@ -1,13 +1,16 @@
 import pytest
 from src.gastos import adicionar_gasto, total_gastos
 
+
 def test_adicionar_valido():
     adicionar_gasto(10)
     assert total_gastos() >= 10
 
+
 def test_valor_negativo():
     with pytest.raises(ValueError):
         adicionar_gasto(-1)
+
 
 def test_total():
     adicionar_gasto(5)
