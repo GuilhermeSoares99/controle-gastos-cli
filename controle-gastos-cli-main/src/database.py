e913a831a4ffdc8200dc35e55d98394eee0bd5c8
GuilhermeSoares99 <guilherme.gs@sempreceub.com>
2026-06-14 17:03:29 -0300

    Fix: move credenciais para o .env e remove chaves expostas

--- controle-gastos-cli-main/src/database.py
+++ controle-gastos-cli-main/src/database.py
@@ -1,8 +1,11 @@
-from supabase import create_client
 import os
+from supabase import create_client
+from dotenv import load_dotenv
 
-# Sua URL e Chave Secreta do Supabase prontas para o projeto
-URL = os.getenv("SUPABASE_URL", "https://tdeicixvfdnlukloxfby.supabase.co")
-KEY = os.getenv("SUPABASE_KEY", "sb_secret_BW-ZzcXJIKiX04ykJLvR7A_xxe91j2S")
+
+load_dotenv()
+
+URL = os.getenv("SUPABASE_URL")
+KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(URL, KEY)
